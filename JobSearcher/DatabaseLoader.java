0e4637f58c9a66cfdc9424c3b968ccf54f1b09d3
Siddharth Gianey <siddharth@Siddharths-MacBook-Pro.local>
2021-10-19 09:14:08 -0400

    Started Json Parsing

--- JobSearcher/DatabaseLoader.java
+++ JobSearcher/DatabaseLoader.java
@@ -1,5 +1,15 @@
 package JobSearcher;
 
-public class DatabaseLoader {
-    
+import java.util.ArrayList;
+import java.io.FileReader;
+public class DatabaseLoader extends DatabaseConstants {
+  public static ArrayList<Company> loadCompanies() {
+    ArrayList<Company> companies = new ArrayList<>();
+
+    try {
+      FileReader reader = new FileReader(COMPANIES_FILE);
+    } catch (Exception e) {
+
+    }
+  }
 }
