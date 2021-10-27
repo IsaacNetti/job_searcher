60a73a98413ee3fdd1c512477dd82991fbcd14a6
Isaac Netti <isaacnetti@gmail.com>
2021-10-26 23:32:23 -0400

    Creating stubs

--- JobSearcher/JobSystem.java
+++ JobSearcher/JobSystem.java
@@ -2,4 +2,49 @@ package JobSearcher;
 
 public class JobSystem {
     
+    public JobSystem(){
+
+    }
+    public void createUser(){
+
+    }
+    public void deleteUser(){
+
+    }
+    public void setAdmin(){
+
+    }
+    public void removeAdmin(){
+
+    }
+    public void createJob(){
+
+    }
+    public void removeJob(){
+
+    }
+    public void apply(){
+
+    }
+    public void rateUser(){
+
+    }
+    public void rejectApplicant(){
+
+    }
+    public void createResume(){
+
+    }
+    public void addExperience(){
+
+    }
+    public void searchJobs(){
+
+    }
+    public void searchCompany(){
+
+    }
+    public void displayApplicants(){
+        
+    }
 }
