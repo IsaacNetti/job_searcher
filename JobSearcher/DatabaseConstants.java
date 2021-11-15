4aaafb6e8e5f4b2e0dcc95c03e91cb3a6a6eee8d
Siddharth Gianey <siddharth@Siddharths-MacBook-Pro.local>
2021-11-14 20:46:01 -0500

    switched the file name constants back after testing

--- JobSearcher/DatabaseConstants.java
+++ JobSearcher/DatabaseConstants.java
@@ -4,12 +4,12 @@ package JobSearcher;
  * @author The Back Rowers
  */
 public abstract class DatabaseConstants {
-  protected static final String ADMINS_FILE = "/Users/siddharth/Documents/CSCE 247 /job_searcher/JobSearcher/datastorage/admins.json";
+  protected static final String ADMINS_FILE = "JobSearcher/datastorage/admins.json";
   protected static final String ADMINS_ID = "id";
   protected static final String ADMINS_USERNAME = "username";
   protected static final String ADMINS_PASSWORD = "password";
 
-  protected static final String EMPLOYERS_FILE = "/Users/siddharth/Documents/CSCE 247 /job_searcher/JobSearcher/datastorage/employers.json";
+  protected static final String EMPLOYERS_FILE = "JobSearcher/datastorage/employers.json";
   protected static final String EMPLOYERS_ID = "id";
   protected static final String EMPLOYERS_USERNAME = "username";
   protected static final String EMPLOYERS_PASSWORD = "password";
@@ -19,7 +19,7 @@ public abstract class DatabaseConstants {
   protected static final String EMPLOYERS_COMPANY = "company";
   protected static final String EMPLOYERS_PHONE_NUMBER = "phoneNumber";
 
-  protected static final String STUDENTS_FILE = "/Users/siddharth/Documents/CSCE 247 /job_searcher/JobSearcher/datastorage/students.json";
+  protected static final String STUDENTS_FILE = "JobSearcher/datastorage/students.json";
   protected static final String STUDENTS_ID = "id";
   protected static final String STUDENTS_USERNAME = "username";
   protected static final String STUDENTS_PASSWORD = "password";
@@ -45,15 +45,14 @@ public abstract class DatabaseConstants {
   protected static final String EXPERIENCES_END  = "end";
   protected static final String EXPERIENCES_DESCRIPTION = "description";
 
-  // protected static final String COMPANIES_FILE = "JobSearcher/datastorage/companies.json";
-  protected static final String COMPANIES_FILE = "/Users/siddharth/Documents/CSCE 247 /job_searcher/JobSearcher/datastorage/companies.json";
+  protected static final String COMPANIES_FILE = "JobSearcher/datastorage/companies.json";
   protected static final String COMPANIES_ID = "id";
   protected static final String COMPANIES_NAME = "name";
   protected static final String COMPANIES_LOCATION = "location";
   protected static final String COMPANIES_SECTOR = "sector";
   protected static final String COMPANIES_INDUSTRY = "industry";
 
-  protected static final String JOBS_FILE = "/Users/siddharth/Documents/CSCE 247 /job_searcher/JobSearcher/datastorage/jobs.json";
+  protected static final String JOBS_FILE = "JobSearcher/datastorage/jobs.json";
   protected static final String JOBS_ID = "id";
   protected static final String JOBS_NAME = "name";
   protected static final String JOBS_COMPANY_ID = "companyid";
@@ -65,7 +64,7 @@ public abstract class DatabaseConstants {
   protected static final String JOBS_LOCATION = "location";
   protected static final String JOBS_REMOTE = "remote";
   
-  protected static final String APPLICATIONS_FILE = "/Users/siddharth/Documents/CSCE 247 /job_searcher/JobSearcher/datastorage/applications.json";
+  protected static final String APPLICATIONS_FILE = "JobSearcher/datastorage/applications.json";
   protected static final String APPLICATIONS_ID = "id";
   protected static final String APPLICATIONS_JOB_ID = "jobID";
   protected static final String APPLICATIONS_STUDENT_ID = "studentID";
