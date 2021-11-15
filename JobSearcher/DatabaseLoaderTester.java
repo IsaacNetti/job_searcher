3f489d52f941859605c6cf9d1afef294fc83d8bd
Christian Lee <christianlee1049@gmail.com>
2021-11-14 20:03:12 -0500

    final testing

--- JobSearcher/DatabaseLoaderTester.java
+++ JobSearcher/DatabaseLoaderTester.java
@@ -1,11 +1,14 @@
 package JobSearcher;
-
 import java.util.ArrayList;
-import 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 public class DatabaseLoaderTester {
     private Users student = Users.getInstance();
     private ArrayList<Student> studentList = student.getStudents();
-
+    @BeforeEach
     public void setup(){
         studentList.clear();
         Student newStudent = new Student();
@@ -16,23 +19,23 @@ public class DatabaseLoaderTester {
         newStudent.setGpa("3.0");
         newStudent.setEduAccount("tagS@email.sc.edu");
     }
-
+    @AfterEach
     public void reset(){
         Users.getInstance().getStudents().clear();
         DatabaseWriter.saveStudents();
     }
-
+    @Test
     void testGetStudentsSize(){
         studentList = DatabaseLoader.loadStudents();
         assertEquals(3, studentList.size());
     }
-
+    @Test
     void testGetStudentsSizeZero(){
         Users.getInstance().getStudents().clear();
         DatabaseWriter.saveStudents();
         assertEquals(0, studentList.size());
     }
-
+    @Test
     void testGetUserLastName(){
         studentList = DatabaseLoader.loadStudents();
         assertEquals("Smith",studentList.get(0).getLastName());
--- JobSearcher/DatabaseWriterTester.java
+++ JobSearcher/DatabaseWriterTester.java
@@ -1,31 +1,41 @@
 package JobSearcher;
 import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 public class DatabaseWriterTester {
     private Users users = Users.getInstance();
     private ArrayList<Student> studentList = users.getStudents();
-
+    @BeforeEach
     public void setup(){
         Users.getInstance().getStudents().clear();
         DatabaseWriter.saveStudents();
     }
-
+    @AfterEach
     public void reset(){
         Users.getInstance().getStudents().clear();
         DatabaseWriter.saveStudents();
     }
-
+    @Test
     void testWritingZeroUsers(){
         studentList = DatabaseLoader.loadStudents();
         assertEquals(0,studentList.size());
     }
-
+    @Test
     void testWritingOneUser(){
-        studentList.add((new Student("tag01", "pop123","Tag","Smith")));
+        Student newStudent = new Student();
+        newStudent.setUsername("tag01");
+        newStudent.setPassword("pop123");
+        newStudent.setFirstName("Tag");
+        newStudent.setLastName("Smith");
+        newStudent.setGpa("3.0");
+        newStudent.setEduAccount("tagS@email.sc.edu");
         DatabaseWriter.saveStudents();
         assertEquals("tag01", DatabaseLoader.loadStudents().get(0).getUsername());
     }
-
+    @Test
     void testWritingFourUsers(){
         Student newStudent = new Student();
         newStudent.setUsername("tag01");
@@ -58,17 +68,31 @@ public class DatabaseWriterTester {
         newStudent.setLastName("Smith");
         newStudent.setGpa("3.0");
         newStudent.setEduAccount("tagS@email.sc.edu");
+
+        DatabaseWriter.saveStudents();
+        assertEquals("tag01",DatabaseLoader.loadStudents().get(2).getUsername());
     } 
-
+    @Test
     void testWritingEmptyUser(){
-
-        studentList.add(new Student("","","","",""));
+        Student newStudent = new Student();
+        newStudent.setUsername("");
+        newStudent.setPassword("");
+        newStudent.setFirstName("");
+        newStudent.setLastName("");
+        newStudent.setGpa("");
+        newStudent.setEduAccount("");
         DatabaseWriter.saveStudents();
         assertEquals("",DatabaseLoader.loadStudents().get(0).getUsername());
     }
-
+    @Test
     void testWritingNullUser(){
-        studentList.add(new Student(null,"","",0,""));
+        Student newStudent = new Student();
+        newStudent.setUsername(null);
+        newStudent.setPassword("");
+        newStudent.setFirstName("");
+        newStudent.setLastName("");
+        newStudent.setGpa("");
+        newStudent.setEduAccount("");
         DatabaseWriter.saveStudents();
         assertEquals(null,DatabaseLoader.loadStudents().get(0).getUsername());
     }
