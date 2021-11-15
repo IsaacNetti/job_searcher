ce5424d3787f0a32c72938030c5baed56cd27c0d
Christian Lee <christianlee1049@gmail.com>
2021-11-14 20:26:26 -0500

    testing

--- JobSearcher/DatabaseLoaderTester.java
+++ JobSearcher/DatabaseLoaderTester.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 public class DatabaseLoaderTester {
     private Users student = Users.getInstance();
     private ArrayList<Student> studentList = student.getStudents();
+
     @BeforeEach
     public void setup(){
         studentList.clear();
@@ -18,6 +19,24 @@ public class DatabaseLoaderTester {
         newStudent.setLastName("Smith");
         newStudent.setGpa("3.0");
         newStudent.setEduAccount("tagS@email.sc.edu");
+
+        Student newStudent1 = new Student();
+        newStudent.setUsername("johnT");
+        newStudent.setPassword("soda444");
+        newStudent.setFirstName("John");
+        newStudent.setLastName("Travolta");
+        newStudent.setGpa("2.9");
+        newStudent.setEduAccount("johnT@email.sc.edu");
+
+        Student newStudent2 = new Student();
+        newStudent.setUsername("frankeatsbeans");
+        newStudent.setPassword("pinto4life");
+        newStudent.setFirstName("Frank");
+        newStudent.setLastName("Sint");
+        newStudent.setGpa("4.0");
+        newStudent.setEduAccount("sint11@email.sc.edu");
+
+        DatabaseWriter.saveStudents();
     }
     @AfterEach
     public void reset(){
@@ -38,6 +57,6 @@ public class DatabaseLoaderTester {
     @Test
     void testGetUserLastName(){
         studentList = DatabaseLoader.loadStudents();
-        assertEquals("Smith",studentList.get(0).getLastName());
+        assertEquals("Smith", studentList.get(0).getLastName());
     }
 }
